9236243d2b31673243070efa92a1aa2727269b53
zhongtaiwan <6fdmw9yk4f@privaterelay.appleid.com>
2026-09-05 21:12:55 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  schoolName: "苗栗縣立照南國民中學",
+  systemTitle: "115學年度第一學期 課表查詢系統",
+  currentSemester: "115-1",
+  timetableFile: "timetable_115_1.csv",
+  homeroomFile: "homerooms_115_1.json",
+  periods: [
+    { id: "1", name: "第 1 節", time: "08:20~09:05" },
+    { id: "2", name: "第 2 節", time: "09:15~10:00" },
+    { id: "3", name: "第 3 節", time: "10:10~10:55" },
+    { id: "4", name: "第 4 節", time: "11:05~11:50" },
+    { id: "5", name: "第 5 節", time: "13:00~13:45" },
+    { id: "6", name: "第 6 節", time: "13:55~14:40" },
+    { id: "7", name: "第 7 節", time: "14:55~15:40" }
+  ]
+};
